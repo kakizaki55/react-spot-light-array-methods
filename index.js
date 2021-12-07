5e77d466524d1d3a8064f173fad4928a9290f503
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-12-07 10:17:54 -0800

    testone passing

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+export const getCountryNames = (countries) => {
+  const countryName = countries.map((c) => c.name);
+  return countryName;
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
